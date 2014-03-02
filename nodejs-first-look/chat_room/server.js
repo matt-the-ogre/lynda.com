1b5449b5944cfe5871199467817e988e91515fbc
Matt Manuel <matt.the.ogre@gmail.com>
2014-03-02 15:34:21 -0800

    little chat application from lynda.com using websockets in node

---
+++ nodejs-first-look/chat_room/server.js
@@ -0,0 +1,16 @@
+var io = require('socket.io'),
+    connect = require('connect'),
+    chatter = require('./chatter.js')
+    cookie = require('cookie');
+    
+var app = connect().use(connect.static('public')).listen(3000);
+var chat_room = io.listen(app);
+
+chatter.set_sockets(chat_room.sockets);
+
+chat_room.sockets.on('connection', function (socket) {
+    chatter.connect_chatter({
+        socket: socket,
+        username: socket.id
+    });
+});
